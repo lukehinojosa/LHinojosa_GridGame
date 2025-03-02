0a94d394fd59ebe4aaf1006b99ecc1f5d38f63a8
lukehinojosa <lukehinojosa05@gmail.com>
2025-03-01 23:50:01 -0500

    Move and Combine Up and Down

--- GridGame/Assets/Scripts/GameManager.cs
+++ GridGame/Assets/Scripts/GameManager.cs
@@ -68,37 +68,52 @@ public class GameManager : MonoBehaviour
 
     void Move(KeyCode direction)
     {
+        int addNumRow = 0;
+        int addNumCol = 0;
+        int startingPosition = 0;
+        int outsideMax = 0;
+        int insideMax = 0;
+        bool horizontal = true;
+
         switch (direction)
         {
             case KeyCode.LeftArrow:
             {
-                CaseHorizontal(direction);
+                addNumCol = -1;
+                startingPosition = 1;
+                outsideMax = _gridManager._rows;
+                insideMax = _gridManager._columns;
                 break;
             }
             case KeyCode.RightArrow:
             {
-                CaseHorizontal(direction);
+                addNumCol = 1;
+                startingPosition = _gridManager._columns - 2;
+                outsideMax = _gridManager._rows;
+                insideMax = _gridManager._columns;
+                break;
+            }
+            case KeyCode.DownArrow:
+            {
+                addNumRow = -1;
+                startingPosition = 1;
+                outsideMax = _gridManager._columns;
+                insideMax = _gridManager._rows;
+                horizontal = false;
+                break;
+            }
+            case KeyCode.UpArrow:
+            {
+                addNumRow = 1;
+                startingPosition = _gridManager._rows - 2;
+                outsideMax = _gridManager._columns;
+                insideMax = _gridManager._rows;
+                horizontal = false;
                 break;
             }
         }
-    }
-
-    void CaseHorizontal(KeyCode direction)
-    {
-        int addNum = 0;
-        int startingColumn = 0;
-        if (direction == KeyCode.LeftArrow)
-        {
-            addNum = -1;
-            startingColumn = 1;
-        }
-        else if (direction == KeyCode.RightArrow)
-        {
-            addNum = 1;
-            startingColumn = _gridManager._columns - 2;
-        }
         
-        for (int row = 0; row < _gridManager._rows; row++)
+        for (int outside = 0; outside < outsideMax; outside++)
         {
             bool blocksMoved = true;
 
@@ -106,50 +121,77 @@ public class GameManager : MonoBehaviour
             {
                 blocksMoved = false;
 
+                int addNum;
+                if (horizontal)
+                    addNum = addNumCol;
+                else
+                    addNum = addNumRow;
+
                 int size = 1;
-                for (int column = startingColumn; size < _gridManager._columns; column -= addNum)
+                for (int inside = startingPosition; size < insideMax; inside -= addNum)
                 {
+                    int row;
+                    int column;
+
+                    if (horizontal)
+                    {
+                        row = outside;
+                        column = inside;
+                    }
+                    else
+                    {
+                        row = inside;
+                        column = outside;
+                    }
+                    
                     size++;
                     
                     if (_objects[row][column] != null)
                     {
-                        if (_objects[row][column + addNum] == null)
+                        if (_objects[row + addNumRow][column + addNumCol] == null)
                         {
                             blocksMoved = true;
-                            MoveBlockToBlankHorizontal(row, column, addNum);
+                            MoveBlockToBlank(row, column, addNumRow, addNumCol);
                         }
-                        else if (!_objects[row][column + addNum].GetComponent<BlockObject>()._combined &&
-                                 _objects[row][column + addNum].GetComponent<BlockObject>().GetBlockNumber() ==
+                        else if (!_objects[row + addNumRow][column + addNumCol].GetComponent<BlockObject>()._combined &&
+                                 _objects[row + addNumRow][column + addNumCol].GetComponent<BlockObject>().GetBlockNumber() ==
                                  _objects[row][column].GetComponent<BlockObject>().GetBlockNumber())
                         {
                             blocksMoved = true;
-                            _objects[row][column + addNum].GetComponent<BlockObject>()._combined = true;
-                            CombineHorizontal(row, column, addNum);
+                            _objects[row + addNumRow][column + addNumCol].GetComponent<BlockObject>()._combined = true;
+                            Combine(row, column, addNumRow, addNumCol);
                         }
+                    }
+
+                    if (size >= insideMax)
+                    {
+                        for (int i = 0; i < _gridManager._rows; i++)
+                            for (int j = 0; j < _gridManager._columns; j++)
+                                if (_objects[i][j] != null)
+                                    _objects[i][j].GetComponent<BlockObject>()._combined = false;
                     }
                 }
             }
-            
-            //Reset all blocks in a row combined state to false
-            for (int column = 0; column < _gridManager._columns - 1; column++)
-                if (_objects[row][column] != null)
-                    _objects[row][column].GetComponent<BlockObject>()._combined = false;
         }
     }
     
-    void MoveBlockToBlankHorizontal(int row, int column, int addNum)
+    void MoveBlockToBlank(int row, int column, int addNumRow, int addNumCol)
     {
-        _objects[row][column + addNum] = _objects[row][column];
-        _objects[row][column + addNum].GetComponent<BlockObject>()._gridPos.x += addNum;
+        _objects[row + addNumRow][column + addNumCol] = _objects[row][column];
+        
+        if (addNumRow != 0)
+            _objects[row + addNumRow][column + addNumCol].GetComponent<BlockObject>()._gridPos.y += addNumRow;
+        else
+            _objects[row + addNumRow][column + addNumCol].GetComponent<BlockObject>()._gridPos.x += addNumCol;
 
-        _objects[row][column + addNum].GetComponent<BlockObject>().UpdatePosition();
+        _objects[row + addNumRow][column + addNumCol].GetComponent<BlockObject>().UpdatePosition();
         _objects[row][column] = null;
     }
     
-    void CombineHorizontal(int row, int column, int addNum)
+    void Combine(int row, int column, int addNumRow, int addNumCol)
     {
-        _objects[row][column + addNum].GetComponent<BlockObject>().DoubleNumber();
-        _score += _objects[row][column + addNum].GetComponent<BlockObject>().GetBlockNumber();
+        _objects[row + addNumRow][column + addNumCol].GetComponent<BlockObject>().DoubleNumber();
+        _score += _objects[row + addNumRow][column + addNumCol].GetComponent<BlockObject>().GetBlockNumber();
         Destroy(_objects[row][column]);
         _objects[row][column] = null; //Destroy is not quick enough
         _objectCount--;
